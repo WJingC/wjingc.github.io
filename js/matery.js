fb92bb2d34c0c0f3c2bcc2e38adaa918700d02ea
wjingc <1293480639@qq.com>
2020-03-27 11:42:37 +0800

    Site updated: 2020-03-27 11:42:36

--- js/matery.js
+++ js/matery.js
@@ -46,7 +46,7 @@ $(function () {
      * 修复样式.
      */
     let fixStyles = function () {
-        fixPostCardWidth('navContainer');
+        fixPostCardWidth('navContainer', 'articles');
         fixPostCardWidth('artDetail', 'prenext-posts');
         fixFooterPosition();
     };
@@ -127,29 +127,21 @@ $(function () {
     /*监听滚动条位置*/
     let $nav = $('#headNav');
     let $backTop = $('.top-scroll');
-    // 当页面处于文章中部的时候刷新页面，因为此时无滚动，所以需要判断位置,给导航加上绿色。
-    showOrHideNavBg($(window).scrollTop());
     $(window).scroll(function () {
         /* 回到顶部按钮根据滚动条的位置的显示和隐藏.*/
         let scroll = $(window).scrollTop();
-        showOrHideNavBg(scroll);
-    });
-
-    function showOrHideNavBg(position) {
-        let showPosition = 100;
-        if (position < showPosition) {
+        if (scroll < 100) {
             $nav.addClass('nav-transparent');
             $backTop.slideUp(300);
         } else {
             $nav.removeClass('nav-transparent');
             $backTop.slideDown(300);
         }
-    }
-
-    	
-	$(".nav-menu>li").hover(function(){
-		$(this).children('ul').stop(true,true).show();
-		 $(this).addClass('nav-show').siblings('li').removeClass('nav-show');
+    });
+    /*支持二级菜单*/
+    $(".nav-menu>li").hover(function(){
+	$(this).children('ul').stop(true,true).show();
+	$(this).addClass('nav-show').siblings('li').removeClass('nav-show');
 		
 	},function(){
 		$(this).children('ul').stop(true,true).hide();
